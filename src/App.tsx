24dbc08737a6cd8051320a28b619a46ff3ea2197
Avir Karakitsos <avir.karakitsos@gmail.com>
2025-01-21 12:32:15 +0100

    feat: header part

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,33 @@
 import './App.css'
+import Header from './components/Header'
 
 function App() {
 
   return (
     <>
-      <h1>Le Bazar d'Avir</h1>
+	<Header/>
+	<div className='headerComponent'>
+		<h1>Le Bazar d'Avir</h1>
+		<p>Trouvez votre objets sur les différentes plateformes</p>
+		<div>
+			<ul className='headerList'>
+				<li>vinted</li>
+				<li>rakuten</li>
+				<li>ebay</li>
+				<li>leboncoin</li>
+			</ul>
+		</div>
+	</div>
+	<div className='categories'>
+		<ul className='categoriesList'>
+			<li>En vrac</li>
+			<li>Livres</li>
+			<li>Cartes à collectionner</li>
+			<li>Timbres</li>
+			<li>Pièces de monnaies</li>
+		</ul>
+	</div>
+	<div className='banner'></div>
     </>
   )
 }
---
+++ src/components/Header.tsx
@@ -0,0 +1,16 @@
+import styles from '../assets/styles/Header.module.css'
+
+function Header() {
+
+  return (
+    <>
+        <div className={styles.container}>
+            <div>logo</div>      
+            <h1>Header Part</h1>
+            <span>likes</span>
+        </div>
+    </>
+  )
+}
+
+export default Header
