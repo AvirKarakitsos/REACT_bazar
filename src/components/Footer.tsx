a5ded3ed34029b20563397635ea0c001f092ec57
Avir Karakitsos <avir.karakitsos@gmail.com>
2025-01-22 14:35:02 +0100

    feat: footer part

---
+++ src/components/Footer.tsx
@@ -0,0 +1,14 @@
+import styles from '../assets/styles/Footer.module.css'
+
+function Footer() {
+
+  return (
+    <>
+        <div className={styles.container}>     
+            <h1>Footer Part</h1>
+        </div>
+    </>
+  )
+}
+
+export default Footer
